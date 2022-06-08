85f9e9322adeeb95c29cdf8d6f5bc23043c31dbb
chrisYiangou <chrisyiangou100@gmail.com>
2022-06-08 17:20:37 +0100

    Simple Buttons added some style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Buttons from './components/Buttons';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className='container--element'>
       <Header />
+      <Buttons />
       
     </div>
   )
---
+++ src/components/Buttons.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Buttons() {
+  return (
+    <div className="buttons--container">
+        <button className="email--button">
+            Email
+        </button>
+        <button className="linkedin-button">
+            Linkedin
+        </button>
+    </div>
+  )
+    
+}
