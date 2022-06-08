47c46d3d7117bf18a59dd66f8565d4dfcb9e63a7
chrisYiangou <chrisyiangou100@gmail.com>
2022-06-08 17:14:58 +0100

    Made the initial changes will fix design later

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ export default function Header() {
     return (
         <div className="header--element">
             <img className="header--img" src={ChrisPic} alt="A pic of our protaganist on a train"></img>
-            <h1 className="header--title--element">Christopher Yiangou</h1>
-            <h3>Software Training Specialist</h3>
+            <h1 >Christopher Yiangou</h1>
+            <h3 className="header--title--element">Software Training Specialist</h3>
             <h4><a href="#">A link to my website (One day in the future)</a></h4>
         </div>
     )
