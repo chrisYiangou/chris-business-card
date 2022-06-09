b164082d5e5d89ed288401261ce8757506854fc0
chrisYiangou <chrisyiangou100@gmail.com>
2022-06-09 15:49:56 +0100

    footer added with three buttons and horrible background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Buttons from './components/Buttons';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Buttons />
       <MainContent />
+      <Footer />
       
     </div>
   )
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer--container">
+            <button>
+                Twitter
+            </button>
+            <button>
+                Github
+            </button>
+            <button>
+                Instagram
+            </button>
+        </div>
+    )
+}
