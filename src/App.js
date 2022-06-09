9ec85399f487cdf8ff71bb1fc3e8b2f20544c0d9
chrisYiangou <chrisyiangou100@gmail.com>
2022-06-09 15:28:54 +0100

    centered the container with main content added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Buttons from './components/Buttons';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div className='container--element'>
       <Header />
       <Buttons />
+      <MainContent />
       
     </div>
   )
